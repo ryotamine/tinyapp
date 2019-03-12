97d60cc9cf9d541e0061b101356da4ad9916f823
Ryota Mine <ryotamine@gmail.com>
2019-03-12 18:40:19 +0000

    Add second route and template

--- express_server.js
+++ express_server.js
@@ -18,6 +18,11 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/:shortURL", (req, res) => {
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  res.render("urls_show", templateVars);
+});
+
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
