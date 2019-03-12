b805cd7bce3bdf9de7c72770f32e84d94ba8b807
Ryota Mine <ryotamine@gmail.com>
2019-03-12 21:59:54 +0000

    Add POST route to receive the form submission

--- express_server.js
+++ express_server.js
@@ -39,6 +39,11 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
