15d640b2fb7fced0a6c6938a9be91dbbb34533b2
Ryota Mine <ryotamine@gmail.com>
2019-03-12 21:48:05 +0000

    Getting ready for POST requests

--- express_server.js
+++ express_server.js
@@ -4,6 +4,9 @@ var PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 // URL database
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
