0266c593914764a7678ea543ea9ace968c04cefc
Ryota Mine <ryotamine@gmail.com>
2019-03-14 00:43:09 +0000

    Added login and logout

--- express_server.js
+++ express_server.js
@@ -1,7 +1,10 @@
 var express = require("express");
+var cookieParser = require("cookie-parser");
+
 var app = express();
 var PORT = 8080; // default port 8080
 
+app.use(cookieParser());
 app.set("view engine", "ejs");
 
 const bodyParser = require("body-parser");
@@ -19,17 +22,25 @@ app.get("/", (req, res) => {
 
 // add random short URL and its long URL to URL database
 app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase };
+  let templateVars = {
+    username: req.cookies["username"],
+    urls: urlDatabase
+  };
   res.render("urls_index", templateVars);
 });
 
 // GET long URL from user
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  let templateVars = { username: req.cookies["username"] };
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  let templateVars = {
+    shortURL: req.params.shortURL,
+    longURL: urlDatabase[req.params.shortURL],
+    username: req.cookies["username"]
+  };
   res.render("urls_show", templateVars);
 });
 
@@ -59,6 +70,18 @@ app.post("/urls/:shortURL", (req, res) => {
 app.post("/urls/:shortURL/delete", (req, res) => {
   delete urlDatabase[req.params.shortURL];
   res.redirect("/urls");
+});
+
+// POST username in cookies
+app.post("/login", (req, res) => {
+  res.cookie("username", req.body.username);
+  res.redirect("/urls/new");
+});
+
+// POST logout and remove cookies
+app.post("/logout", (req, res) => {
+  res.clearCookie("username");
+  res.redirect("/urls/new");
 });
 
 // GET random short URL to open long URL
